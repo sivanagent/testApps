bbe6b670cc4c10cc53a23ad9fab07a5a28733e32
dvenk <dvenk@APPA-USA-CMPTR>
2017-05-01 18:25:32 +0530

    Add project files.

---
+++ testApps/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Security;
+using System.Text;
+using System.Web.Services.Description;
+using System.Xml;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+
+
+namespace testApps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //download  remote file FROM remote ACIAF server
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /**
+            //Serializer ss = new Serializer( "d:\\temp\\", "SerializerTest.xml");
+            //ss.testSerialize();
+
+            aciafServiceReference.ACIAFClient clntAciaf = new aciafServiceReference.ACIAFClient();
+            //staAciaf.
+            aciafServiceReference.RemoteACIAFAgentInstanceArchiveFileInfo fileInfo = new aciafServiceReference.RemoteACIAFAgentInstanceArchiveFileInfo();
+            fileInfo.FileName = @"d:\abc.txt";
+            var FileByteStream = new Object();
+
+            System.IO.Stream fileStream; 
+
+            clntAciaf.SendAgentInstanceArchiveToRemoteACIAFServer(ref fileInfo.FileName, out fileStream);
+
+            byte[] buffer;
+           
+            try
+            {
+                //int length = (int)fileStream.Length;  // get file length
+                buffer = new byte[11500];            // create buffer
+                int count;                            // actual number of bytes read
+                int sum = 0;                          // total number of bytes read
+                int i = 0;
+
+                // read until Read method returns 0 (end of the stream has been reached)
+                while ((count = fileStream.Read(buffer, sum, 500)) > 0)
+                {
+                    Console.WriteLine("hello............{0}.", i);
+                    
+                    Console.WriteLine(System.Text.Encoding.UTF8.GetString(buffer));
+                    sum += count;  // sum is a buffer offset for next reading
+                    if (fileStream.CanRead != true)
+                    {
+                        break;
+                    }
+                }
+            }
+            finally
+            {
+                fileStream.Close();
+            }
+
+            ***/           
+            try
+            {
+            //http://localhost/serviceACIAF/ACIAF.svc?wsdl
+            //serviceACIAF.ACIAF serviceAciaf = new ACIAFClient();
+
+            Uri mexAddress = new Uri(@"http://localhost/serviceACIAF/ACIAF.svc?wsdl");
+            MetadataExchangeClientMode mexMode = MetadataExchangeClientMode.HttpGet;
+            string contractName = "IACIAF";
+            string operationNameSendAgent = "SendAgentInstanceArchiveToRemoteACIAFServer";
+            string operationNameReceiveAgent = "ReceiveAgentInstanceArchiveFROMRemoteACIAFServer";
+
+            serviceACIAF.DownloadArchivedAgentRequest dwnld_AgentFile_Request = new serviceACIAF.DownloadArchivedAgentRequest();
+            dwnld_AgentFile_Request.FileName = @"d:\abc.txt" ;
+            
+
+            MetadataExchangeClient mexClient = new MetadataExchangeClient(mexAddress, mexMode);
+            mexClient.ResolveMetadataReferences = true;
+            MetadataSet metaSet = mexClient.GetMetadata();
+
+            WsdlImporter importer = new WsdlImporter(metaSet);
+                
+            //BEGIN ............
+            XsdDataContractImporter xsd = new XsdDataContractImporter();
+            xsd.Options = new ImportOptions();
+            xsd.Options.ImportXmlType = true;
+            xsd.Options.GenerateSerializable = true;
+            //xsd.Options.ReferencedTypes.Add(typeof(KeyValuePair<string, string>));
+            //xsd.Options.ReferencedTypes.Add(typeof(System.Collections.Generic.List<KeyValuePair<string, string>>));
+            //xsd.Options.ReferencedTypes.Add(typeof(System.Collections.Generic.List<Addition.MathData>));
+
+            xsd.Options.ReferencedTypes.Add(typeof(serviceACIAF.DownloadArchivedAgentRequest));
+            xsd.Options.ReferencedTypes.Add(typeof(serviceACIAF.RemoteACIAFAgentInstanceArchiveFileInfo));
+            xsd.Options.ReferencedTypes.Add(typeof(System.IO.Stream));
+
+                importer.State.Add(typeof(XsdDataContractImporter), xsd);
+            //END ..............
+
+
+            Collection<ContractDescription> contracts = importer.ImportAllContracts();
+            ServiceEndpointCollection allEndpoints = importer.ImportAllEndpoints();
+
+            ServiceContractGenerator generator = new ServiceContractGenerator();
+            var endpointsForContracts = new Dictionary<string, IEnumerable<ServiceEndpoint>>();
+
+            foreach (ContractDescription contract in contracts)
+            {
+                generator.GenerateServiceContractType(contract);
+                endpointsForContracts[contract.Name] = allEndpoints.Where(
+                    se => se.Contract.Name == contract.Name).ToList();
+            }
+
+            if (generator.Errors.Count != 0)
+                throw new Exception("There were errors during code compilation.");
+
+            CodeGeneratorOptions options = new CodeGeneratorOptions();
+            options.BracingStyle = "C";
+            CodeDomProvider codeDomProvider = CodeDomProvider.CreateProvider("C#");
+
+            CompilerParameters compilerParameters = new CompilerParameters(
+                new string[] {
+            "System.dll", "System.ServiceModel.dll",
+            "System.Runtime.Serialization.dll" });
+            compilerParameters.GenerateInMemory = true;
+
+            CompilerResults results = codeDomProvider.CompileAssemblyFromDom(
+                compilerParameters, generator.TargetCompileUnit);
+
+            if (results.Errors.Count > 0)
+            {
+                throw new Exception("There were errors during generated code compilation");
+            }
+            else
+            {
+                    Type clientProxyType = results.CompiledAssembly.GetTypes().First(
+                        t => t.IsClass &&
+                            t.GetInterface(contractName) != null &&
+                            t.GetInterface(typeof(ICommunicationObject).Name) != null);
+
+                    ServiceEndpoint se = endpointsForContracts[contractName].First();
+
+                    object instance = results.CompiledAssembly.CreateInstance(
+                        clientProxyType.Name,
+                        false,
+                        System.Reflection.BindingFlags.CreateInstance,
+                        null,
+                        new object[] { se.Binding, se.Address },
+                        CultureInfo.CurrentCulture, null);
+
+                    var methodInfoReceiveAgent  = instance.GetType().GetMethod(operationNameReceiveAgent);
+                    var methodInfoSendAgent     = instance.GetType().GetMethod(operationNameSendAgent);
+
+                    serviceACIAF.RemoteACIAFAgentInstanceArchiveFileInfo dwnld_agnt_fileInfo = new serviceACIAF.RemoteACIAFAgentInstanceArchiveFileInfo();
+                    //dwnld_agnt_fileInfo.FileName
+                    //dwnld_agnt_fileInfo.Length
+                    //dwnld_agnt_fileInfo.FileByteStream
+                    //dwnld_agnt_fileInfo.
+                    System.IO.Stream fileStream1;
+                    object[] arguments = new object[2];
+
+                    //this expects two arguments ..FIrst "filename" to download from server, next reference to OUT parameter FILESTREAM 
+                    //from aciaf server that this client can use to read the stream and download..hahaha 
+                    arguments[0] = @"d:\\abc.txt";
+                    var retval  = methodInfoSendAgent.Invoke(instance, BindingFlags.InvokeMethod, null, arguments, null);
+                    fileStream1 = (System.IO.Stream) arguments[1];
+
+                    try
+                    {
+                        //int length = (int)fileStream.Length;  // get file length
+                        byte [] buffer = new byte[11500];            // create buffer
+                        int count;                            // actual number of bytes read
+                        int sum = 0;                          // total number of bytes read
+                        int i = 0;
+
+                        // read until Read method returns 0 (end of the stream has been reached)
+                        while ((count = fileStream1.Read(buffer, sum, 500)) > 0)
+                        {
+                            Console.WriteLine("hello............{0}.", i);
+
+                            Console.WriteLine(System.Text.Encoding.UTF8.GetString(buffer));
+                            sum += count;  // sum is a buffer offset for next reading
+                            if (fileStream1.CanRead != true)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        fileStream1.Close();
+                    }
+
+
+
+                    //object retVal1 = methodInfo1.Invoke(instance, BindingFlags.InvokeMethod, null, operationParameters????, null);
+                    Console.WriteLine(retval.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                //error in dynamic webservice invocation...
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //upload local file to remote ACIAF server
+
+            aciafServiceReference.RemoteACIAFAgentInstanceArchiveFileInfo aa = new aciafServiceReference.RemoteACIAFAgentInstanceArchiveFileInfo();
+            aa.FileByteStream   = ;
+            aa.FileName         = ;
+            aa.Length = ;
+        }
+    }
+}
---
+++ testApps/SerializerHelper.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace testApps
+{
+    [Serializable()]
+    public class Garage
+    {
+        private Vehicle _MyVehicle1;
+        private Vehicle _MyVehicle2;
+
+        public List<Vehicle> oListOfVehicles = new List<Vehicle> ();
+
+        public Garage()
+        {
+        }
+        public string GarageOwner { get; set; }
+        public Vehicle MyVehicle1
+        {
+            get { return _MyVehicle1; }
+            set { _MyVehicle1 = value; }
+        }
+
+        public Vehicle MyVehicle2
+        {
+            get { return _MyVehicle2; }
+            set { _MyVehicle2 = value; }
+        }
+
+    }
+
+    [XmlInclude(typeof(Car))]
+    [XmlInclude(typeof(Boat))]
+    [XmlInclude(typeof(Motorcycle))]
+    [XmlInclude(typeof(Motorhome))]
+    public abstract class Vehicle
+    {
+        public string VehicleType { get; set; }
+        public int VehicleNumber { get; set; }
+    }
+    public class Car : Vehicle
+    {
+        public int Doors { get; set; }
+    }
+    public class Boat : Vehicle
+    {
+        public int Engines { get; set; }
+    }
+    public class Motorcycle : Vehicle
+    {
+        public int Wheels { get; set; }
+    }
+    public class Motorhome : Vehicle
+    {
+        public int Length { get; set; }
+    }
+
+    class Serializer
+    {
+        static string _StartupPath;
+        static string _StartupFile;
+        static string _StartupXML; 
+
+        public  Serializer() {  }
+
+        public Serializer(string t_StartupPath, string t_StartupFile)
+        {
+            _StartupPath = t_StartupPath;
+            _StartupFile = t_StartupFile;
+            _StartupXML = _StartupPath + _StartupFile;
+        }
+
+
+        //static void Main(string[] args)
+        public void testSerialize()
+        {
+            Console.Write("Press w for write. Press r for read:");
+            ConsoleKeyInfo cki = Console.ReadKey(true);
+            Console.WriteLine("Pressed: " + cki.KeyChar.ToString());
+            if (cki.KeyChar.ToString() == "w")
+            {
+                Garage MyGarage = new Garage();
+                MyGarage.GarageOwner = "xyzJillJohn";
+                Car c = new Car();
+                c.VehicleType = "Lexus";
+                c.VehicleNumber = 1234;
+                c.Doors = 4;
+                MyGarage.MyVehicle1 = c;
+
+                Motorhome m = new Motorhome();
+                m.VehicleType = "abcdMotorHome";
+                m.VehicleNumber = 99991234;
+                m.Length = 44;
+                MyGarage.MyVehicle2 = m;
+
+                MyGarage.oListOfVehicles.Add(c);
+                MyGarage.oListOfVehicles.Add(c);
+                MyGarage.oListOfVehicles.Add(c);
+                MyGarage.oListOfVehicles.Add(m);
+
+                WriteGarageXML(MyGarage);
+                Console.WriteLine("Serialized");
+            }
+            else if (cki.KeyChar.ToString() == "r")
+            {
+                Garage MyGarage = ReadGarageXML();
+                Console.WriteLine("Deserialized Garage owned by " + MyGarage.GarageOwner);
+            }
+            Console.ReadKey();
+        }
+        public static void WriteGarageXML(Garage pInstance)
+        {
+            XmlSerializer writer = new XmlSerializer(typeof(Garage));
+            using (FileStream file = File.OpenWrite(_StartupXML))
+            {
+                writer.Serialize(file, pInstance);
+            }
+
+            /******
+            XmlSerializer x = new XmlSerializer(typeof(ClassOfYOurs));
+            System.IO.Stream s;
+
+            var xmlwriter = System.Xml.XmlWriter.Create("C:\\xmlArchive.txt"); 
+            x.Serialize(xmlwriter, ClassOfYOurs);
+            ****/
+        }
+        public static Garage ReadGarageXML()
+        {
+            XmlSerializer reader = new XmlSerializer(typeof(Garage));
+            using (FileStream input = File.OpenRead(_StartupXML))
+            {
+                return reader.Deserialize(input) as Garage;
+            }
+        }
+    }
+}
